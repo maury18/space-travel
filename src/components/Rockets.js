9652278439d7be942f89309626e89d08d5be68f1
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-08-03 08:45:21 -0400

    Add MissionTable.js and Rockects.js components files

---
+++ src/components/Rockets.js
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+import { updateRocket } from '../redux/rockets';
+
+const Rockets = (props) => {
+  const dispatch = useDispatch();
+
+  const {
+    name, description, image, id, reserved
+  } = props;
+
+  return (
+    <li className="rocket-container">
+      <img className="rocket-container__img" src={image} alt={name} />
+      <div className="rocket-info">
+        <h2 className="rocket-info__title">{name}</h2>
+        <p className="rocket-info__description">
+          {reserved
+            && <span className="rocket-info__description__reserved">Reserved</span>}
+          {description}
+        </p>
+        {reserved ? <button className="rocket-info__cancelbutton" type="button" onClick={() => dispatch(updateRocket(id))}>Cancel reservation</button>
+        : <button className="rocket-info__reservebutton" type="button" onClick={() => dispatch(updateRocket(id))}>Reserve rocket</button>}
+      </div>
+    </li>
+  );
+};
+
+export default Rockets;
